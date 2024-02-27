1adeb8d65f327c91f76478e3c5da6a93d5564b5d
lxfu1 <954055752@qq.com>
2024-02-27 17:58:15 +0800

    feat: merge two lists

--- src/merge-two-lists.rs
+++ src/merge-two-lists.rs
@@ -1,3 +1,10 @@
+/**
+ * 将两个升序链表合并为⼀个新的升序链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 示例：
+ * 输⼊：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+*/
+
 pub struct Node {
   val: Option<i32>,
   next: Option<Box<Node>>,
@@ -51,6 +58,6 @@ fn main() {
   l1.append(Some(2)).append(Some(4));
   let mut l2 = Node::new(Some(1));
   l2.append(Some(3)).append(Some(4));
-  let list: Option<Box<Node>> = merge_two_lists(Some(Box::new(l1)), Some(Box::new(l2)));
-  print_list(list)
+  let merge_list: Option<Box<Node>> = merge_two_lists(Some(Box::new(l1)), Some(Box::new(l2)));
+  print_list(merge_list)
 }
